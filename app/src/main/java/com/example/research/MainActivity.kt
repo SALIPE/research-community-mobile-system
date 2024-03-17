7f5d6e5ce88b504ce05497577ff01cefb80769d2
Felipe Bueno de Souza <felipebuenosouza@uol.com.br>
2024-03-17 18:56:03 +0000

    card

--- app/src/main/java/com/example/research/MainActivity.kt
+++ app/src/main/java/com/example/research/MainActivity.kt
@@ -3,16 +3,26 @@ package com.example.research
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.research.ui.theme.ResearchTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +33,12 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(
+                        name = "Felipe",
+                        age = 22
+                    )
                 }
             }
         }
@@ -32,19 +46,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+fun GreetingText(name: String, modifier: Modifier = Modifier, age: Int = 0) {
+
+    val nameConcat: String = stringResource(R.string.happy_birthday).plus(name)
+    val personAge: Int = calculateAge(age)
+    val congratsMessage = "Congrats for your $personAge age birthday!"
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(5.dp)
+    ) {
         Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
+            text = nameConcat,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+            )
+        Text(
+            text = congratsMessage,
+            fontSize = 30.sp,
+            lineHeight = 30.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(R.string.from_android_team),
+            fontSize = 20.sp,
+            modifier = Modifier.align(alignment = Alignment.End)
         )
     }
+
+}
+
+@Composable
+fun GreetingImage(name: String, age: Int, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+
+    Box(modifier) {
+        Image(painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop)
+        GreetingText(
+            name = "Felipe",
+            age = 22,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+fun calculateAge(age: Int): Int {
+    var initAge: Int = 0
+    initAge += age
+    return initAge
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ResearchTheme {
-        Greeting("Felipe")
+        GreetingImage(
+            name = "Felipe",
+            age = 22
+        )
     }
 }
